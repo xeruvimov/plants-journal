77c6d4fb703f9fcee59db0b1c2de1a6ad1c8115e
etozhebgdn <xeruvimov@gmail.com>
2021-07-15 12:29:04 +0400

    Start project 'plants journal'

---
+++ src/test/java/com/plants/journal/JournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plants.journal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
